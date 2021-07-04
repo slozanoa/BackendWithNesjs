5c0590c183a8a58d94b639f186d194e3a774a6ff
santiago lozano <santiago.lozano@qcode.co>
2021-07-04 13:54:35 -0500

    useFactory

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,29 @@
 /* eslint-disable prettier/prettier */
-import { Module } from '@nestjs/common';
+import { Module, HttpModule, HttpService } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ProductsModule } from './products/products.module';
 
+const API_KEY = '123456';
+const API_KEY_PROD = 'PROD345';
 @Module({
-  imports: [UsersModule, ProductsModule],
+  imports: [HttpModule, UsersModule, ProductsModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: 'API_KEY',
+      useValue: process.env.NODE_ENV === 'prod' ? API_KEY_PROD : API_KEY,
+    },
+    {
+      provide: 'TASKS',
+      useFactory: async (http: HttpService) => {
+        const tasks = await http.get('https://jsonplaceholder.typicode.com/todos/').toPromise();
+        return tasks.data;
+      },
+      inject:[HttpService]  
+    }
+  ],
 })
-export class AppModule {}
+export class AppModule { }
