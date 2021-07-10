59bd3efc38807b304f7e8f39a127fcca3a1c2f7c
santiago lozano <santiago.lozano@qcode.co>
2021-07-10 09:46:44 -0500

    core

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
@@ -14,11 +15,12 @@ async function bootstrap() {
 
   const config = new DocumentBuilder()
     .setTitle('API')
-    .setDescription('PLATZI STORE')
+    .setDescription('store-app')
     .setVersion('1.0')
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('docs', app, document);
+  app.enableCors();
   await app.listen(3000);
 }
 bootstrap();
